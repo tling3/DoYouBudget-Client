ad3e5923c6b2bff5b058e448aa18e8a42c6b2ed9
Tim Ling <tling3@yahoo.com>
2022-02-12 11:24:23 -0800

    added routing and header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Router, Route } from "react-router-dom";
+import history from "../History";
+import PageOne from "./PageOne";
+import Header from "../Shared/Header";
 
 const App = () => {
-  return <div>App Component</div>;
+  return (
+    <Router history={history}>
+      <div>
+        <Header />
+        <Route path="/" exact component={PageOne}></Route>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
